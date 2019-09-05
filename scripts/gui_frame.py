15bcfc25bee463febe7c9228fe7a03baa2c466e7
MickeyKen <mickey333ff@gmail.com>
2019-09-05 17:11:49 +0900

    [Edit]

--- scripts/gui_frame.py
+++ scripts/gui_frame.py
@@ -4,6 +4,8 @@ import sys
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from tutorial import Ui_Form
+from first import Ui_First
+from second import Ui_Second
 import rospy
 from geometry_msgs.msg import Twist
 import rospkg
@@ -33,10 +35,23 @@ class Test(QDialog):
         self.cmd_vel_Twist.linear.x = 0
 
     def create(self):
-
+        ROS_PROGRAM = QProcess(self)
+        print "Launching..."
+        program = 'roslaunch pir2_description pir2_description.launch'
+        ROS_PROGRAM.start(program)
         self.cmd_vel_Twist.angular.z = -1 # 1[rad/s]で右に回転
         self.pub_cmd_vel.publish(self.cmd_vel_Twist)
         self.cmd_vel_Twist.angular.z = 0
+
+    # def create(self):
+    #     position = self.ui.pos()  # 遷移前のdlgの座標を取得
+    #     size = self.ui.size()  # 遷移前のサイズを取得
+    #     self.uif = Ui_First()
+    #     self.uif.move(position.x(), position.y())  # 同じ位置へ
+    #     self.uif.resize(size)  # 同じサイズへ
+    #     # 画面の位置が完全に重なる
+    #     self.uif.show()  # 遷移後のダイアログを表示
+    #     self.ui.hide()  # 遷移前のダイアログを非表示
 
 
 if __name__ == '__main__':
