1ecc5cde0b93ad431d265596ca5500ccc0dfe653
MickeyKen <mickey333ff@gmail.com>
2019-09-22 12:07:01 +0900

    [Fix] image and all functionas

--- scripts/casher_sample.py
+++ scripts/casher_sample.py
@@ -15,24 +15,46 @@ class Ui_Form(object):
         self.dial = QtWidgets.QDial(Form)
         self.dial.setGeometry(QtCore.QRect(100, 60, 481, 431))
         self.dial.setObjectName("dial")
+	self.dial.setStyleSheet('background-color:#f5f5f5')
+
         self.pushButton = QtWidgets.QPushButton(Form)
         self.pushButton.setGeometry(QtCore.QRect(620, 100, 221, 141))
         self.pushButton.setObjectName("pushButton")
+        self.pushButton.setIcon(QtGui.QIcon('drink2.png'))
+        self.pushButton.setIconSize(QtCore.QSize(221,141))
+	self.pushButton.setStyleSheet('background-color:#eee8aa')
+
         self.pushButton_2 = QtWidgets.QPushButton(Form)
         self.pushButton_2.setGeometry(QtCore.QRect(620, 300, 221, 141))
         self.pushButton_2.setObjectName("pushButton_2")
+        self.pushButton_2.setIcon(QtGui.QIcon('noodle2.png'))
+        self.pushButton_2.setIconSize(QtCore.QSize(221,141))
+	self.pushButton_2.setStyleSheet('background-color:#eee8aa')
+
         self.pushButton_3 = QtWidgets.QPushButton(Form)
         self.pushButton_3.setGeometry(QtCore.QRect(880, 100, 221, 141))
         self.pushButton_3.setObjectName("pushButton_3")
+        self.pushButton_3.setIcon(QtGui.QIcon('okashi2.png'))
+        self.pushButton_3.setIconSize(QtCore.QSize(221,141))
+	self.pushButton_3.setStyleSheet('background-color:#eee8aa')
+
         self.pushButton_4 = QtWidgets.QPushButton(Form)
         self.pushButton_4.setGeometry(QtCore.QRect(880, 300, 221, 141))
         self.pushButton_4.setObjectName("pushButton_4")
+        self.pushButton_4.setIcon(QtGui.QIcon('dagashi2.png'))
+        self.pushButton_4.setIconSize(QtCore.QSize(221,141))
+	self.pushButton_4.setStyleSheet('background-color:#eee8aa')
+
         self.pushButton_5 = QtWidgets.QPushButton(Form)
         self.pushButton_5.setGeometry(QtCore.QRect(880, 490, 221, 71))
         self.pushButton_5.setObjectName("pushButton_5")
+	self.pushButton_5.setStyleSheet('background-color:#ffff00')
+
         self.lcdNumber = QtWidgets.QLCDNumber(Form)
         self.lcdNumber.setGeometry(QtCore.QRect(670, 500, 151, 51))
         self.lcdNumber.setObjectName("lcdNumber")
+	self.lcdNumber.setStyleSheet('background-color:#ffffff')
+
         self.label = QtWidgets.QLabel(Form)
         self.label.setGeometry(QtCore.QRect(300, 250, 111, 41))
         self.label.setObjectName("label")
@@ -43,12 +65,12 @@ class Ui_Form(object):
     def retranslateUi(self, Form):
         _translate = QtCore.QCoreApplication.translate
         Form.setWindowTitle(_translate("Form", "Form"))
-        self.pushButton.setText(_translate("Form", "PushButton"))
-        self.pushButton_2.setText(_translate("Form", "PushButton"))
-        self.pushButton_3.setText(_translate("Form", "PushButton"))
-        self.pushButton_4.setText(_translate("Form", "PushButton"))
-        self.pushButton_5.setText(_translate("Form", "PushButton"))
-        self.label.setText(_translate("Form", "TextLabel"))
+        self.pushButton.setText(_translate("Form", ""))
+        self.pushButton_2.setText(_translate("Form", ""))
+        self.pushButton_3.setText(_translate("Form", ""))
+        self.pushButton_4.setText(_translate("Form", ""))
+        self.pushButton_5.setText(_translate("Form", ""))
+        self.label.setText(_translate("Form", "Lee"))
 
 
 if __name__ == "__main__":
--- scripts/frame2.py
+++ scripts/frame2.py
@@ -17,19 +17,97 @@ class Test(QDialog):
         super(Test, self).__init__(parent)
         self.ui = Ui_Form()
         self.ui.setupUi(self)
+
         p = self.palette()
-        p.setColor(self.backgroundRole(), Qt.lightGray)
+        p.setColor(self.backgroundRole(), QColor(190, 190, 190))
         self.setPalette(p)
 
-        self.cmd_vel_Twist = Twist()
-        self.pub_cmd_vel = rospy.Publisher('/turtle1/cmd_vel',Twist,queue_size=10)
+	self.cy = 0
+	self.name = ""
 
-    def start(self):
+        self.ui.dial.setMinimum(0)
+        self.ui.dial.setMaximum(14) 
+        self.ui.dial.setValue(0)
+	self.ui.dial.valueChanged.connect(self.sliderMoved)
+        name = "Lee"
+        self.ui.label.setFont(QFont("Times", 16, QFont.Bold))
+        self.ui.label.setText(name)
 
-        self.cmd_vel_Twist.angular.z = 1 
-        self.pub_cmd_vel.publish(self.cmd_vel_Twist)
-        self.cmd_vel_Twist.angular.z = 0
+        self.ui.pushButton_5.setFont(QFont("ＭＳ 明朝", 22, QFont.Bold))
+        self.ui.pushButton_5.setText("精算")
 
+        # add signals
+        self.ui.pushButton.clicked.connect(self.drink)
+        self.ui.pushButton_2.clicked.connect(self.cup)
+        self.ui.pushButton_3.clicked.connect(self.okashi)
+        self.ui.pushButton_4.clicked.connect(self.dagashi)
+        self.ui.pushButton_5.clicked.connect(self.cash)
+
+    def sliderMoved(self):
+	value = self.ui.dial.value()
+	name=""
+        if value == 0:
+	  name = "Lee"
+	elif value == 1:
+	  name = "yamazoe"
+	elif value == 2:
+	  name = "kuroda"
+	elif value == 3:
+	  name = "Tran"
+	elif value == 4:
+	  name = "kojima"
+	elif value == 5:
+	  name = "fujii"
+	elif value == 6:
+	  name = "Miran"
+	elif value == 7:
+	  name = "Frank"
+	elif value == 8:
+	  name = "Oozono"
+	elif value == 9:
+	  name = "Sei"
+	elif value == 10:
+	  name = "nakamura"
+	elif value == 11:
+	  name = "imazaike"
+	elif value == 12:
+	  name = "imanaka"
+	elif value == 13:
+	  name = "miki"
+	elif value == 14:
+	  name = "yamamoto"
+	else:
+	  pass
+	self.name = name 
+        self.ui.label.setFont(QFont("Times", 16, QFont.Bold))
+        self.ui.label.setText(name)
+
+        print("Dial value = %i" % (self.ui.dial.value()))
+
+    def drink(self):
+	self.cy += 100
+	self.display()
+    def cup(self):
+	self.cy += 100
+	self.display()
+    def okashi(self):
+	self.cy += 100
+	self.display()
+    def dagashi(self):
+	self.cy += 30
+	self.display()
+    def cash(self):
+ 	QMessageBox.information(self, "精算", "名前:  " + self.name + "\n" + "合計: " + str(self.cy) + "円")
+	self.init()
+
+    def init(self):
+	self.cy = 0
+	self.display()
+        self.ui.dial.setValue(0)
+
+
+    def display(self):
+	 self.ui.lcdNumber.display(self.cy)
 
 if __name__ == '__main__':
     rospy.init_node('casher_program')
